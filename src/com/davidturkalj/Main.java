23395e4aa6bbf3ff3e01bee3469b8013f8b6289e
David Turkalj <davidturkalj@gmail.com>
2020-12-03 15:44:30 +0100

    dt-removing number

--- src/com/davidturkalj/Main.java
+++ src/com/davidturkalj/Main.java
@@ -3,6 +3,6 @@ package com.davidturkalj;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Pozdrav iz aplikacije od David Turkalj, broj 2132141241!");
+        System.out.println("Pozdrav iz aplikacije od David Turkalj!");
     }
 }
